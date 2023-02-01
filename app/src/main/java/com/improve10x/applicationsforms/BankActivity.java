744431daf68e45def4e4e2412643e0f2434fab31
surya <surya.natuva@improve10x.com>
2023-02-01 11:13:54 +0530

    new

---
+++ app/src/main/java/com/improve10x/applicationsforms/BankActivity.java
@@ -0,0 +1,15 @@
+package com.improve10x.applicationsforms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank);
+    }
+
+}
---
+++ app/src/main/java/com/improve10x/applicationsforms/SingupActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.applicationsforms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}
